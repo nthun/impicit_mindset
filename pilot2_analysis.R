68dffaf0714de3ac4ac0cefc253be1aa2f069764
Kata Sik <katasik@kata-air.t.hu>
2020-02-20 22:39:43 +0100

    removed no_go_trial means from analysis

--- pilot2_analysis.R
+++ pilot2_analysis.R
@@ -8,7 +8,7 @@ important_targets <- c("chall_pos", "chall_neg", "crit_pos", "crit_neg")
 
 # read data -------------------------------------------------------------------------
 gnat_raw <- 
-  vroom(file = list.files(path = "data/pilot_2/", pattern = ".*.txt",
+  vroom(file = list.files(path = "/Users/katasik/data/", pattern = ".*.txt",
                           full.names = TRUE), 
         col_names = c("block", "block_id", "word", "max_rt", "trial_type", "word_category", "rt", "error", "target"), 
         id = "file")
@@ -26,9 +26,29 @@ gnat_raw %>%
   group_by(file) %>% 
   summarise(correct = 1 - mean(error)) %>% view()
 
+### included no_go_trials
+dscores_wrong <-
+  gnat_raw %>%  
+  filter(target %in% important_targets) %>%
+  group_by(file) %>% 
+  mutate(pers_sd = sd(rt)) %>% 
+  group_by(file, target, pers_sd) %>% 
+  summarise(pers_block_avg = mean(rt)) %>% 
+  ungroup() %>% 
+  mutate(d = pers_block_avg/pers_sd) %>% 
+  separate(target, c("target1", "target2")) %>% 
+  select(-pers_block_avg, -pers_sd) %>% 
+  unite(targets, c("target1", "target2"), sep = "_") %>% 
+  spread(targets, d) %>% 
+  transmute(id = str_remove_all(file, "data/pilot_2/|.txt"),
+            challange_d = chall_neg - chall_pos,
+            crit_d = crit_neg - crit_pos)
+
+### with only go_trials
 dscores <-
   gnat_raw %>%  
   filter(target %in% important_targets) %>%
+  filter(trial_type=="go_trial") %>% 
   group_by(file) %>% 
   mutate(pers_sd = sd(rt)) %>% 
   group_by(file, target, pers_sd) %>% 
@@ -50,5 +70,7 @@ dscores %>%
   
 dscores %>% 
   left_join(explicit, by = "id") %>% 
-  write.xlsx("dscores_explicit.xlsx")
+  write.xlsx("dscoresexplicit.xlsx")
+
+write.xlsx(dscores, "dscores_good_pilot2.xlsx")
 
