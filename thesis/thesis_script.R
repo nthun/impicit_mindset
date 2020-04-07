614dc198b13792378d58748d734b5d5355f286a9
nthun <thomassocska@yahoo.com>
2020-04-07 14:29:22 +0200

    Clean script

--- thesis/thesis_script.R
+++ thesis/thesis_script.R
@@ -69,12 +69,14 @@ explicit_raw <-
   read_excel("thesis/data.xlsx", 1) %>%
   extract(col = participant, 
           into = c(NA, "id", NA), 
-          regex = "^(.*s.)(.*)(.txt)$")
+          regex = "^(.*s.)(.*)(.txt)$") %>% 
+  clean_names()
 
 ##cleaning explicit data
 #reversing items
-explicit_reversed <- explicit_raw %>% 
-  mutate_at(vars(IQ1.1, IQ2.1, FMS3.1, FMS4.1, CrMS3.1, CrMS4.1, ChMS3.1, ChMS4.1), 
+explicit_reversed <- 
+  explicit_raw %>% 
+  mutate_at(vars(iq1_1, iq2_1, fms3_1, fms4_1, cr_ms3_1, cr_ms4_1, ch_ms3_1, ch_ms4_1), 
             ~recode(., `1` = 6,
                     `2` = 5,
                     `3` = 4,
@@ -83,8 +85,9 @@ explicit_reversed <- explicit_raw %>%
                     `6` = 1,
             ))
 
-explicit_reversed <- explicit_reversed %>% 
-  mutate_at(vars(Failurescenario.1, Criticismscenario.1), 
+explicit_reversed <- 
+  explicit_reversed %>% 
+  mutate_at(vars(failurescenario_1, criticismscenario_1), 
             ~recode(., `1` = 5,
                     `2` = 4,
                     `4` = 2,
@@ -92,28 +95,29 @@ explicit_reversed <- explicit_reversed %>%
             ))
 
 #creating means of explicit measures
-explicit_coded<-explicit_reversed %>%
-  mutate(IQMS_M = rowMeans(x = select(.data = .,
-                                      starts_with(match = "IQ"))))
-explicit_coded<-explicit_coded %>%
-  mutate(CRMS_M = rowMeans(x = select(.data = .,
-                                      starts_with(match = "CrMS"))))
-explicit_coded<-explicit_coded %>%
-  mutate(CHMS_M = rowMeans(x = select(.data = .,
-                                      starts_with(match = "ChMS"))))
-explicit_coded<-explicit_coded %>%
-  mutate(FMS_M = rowMeans(x = select(.data = .,
-                                     starts_with(match = "FMS"))))
-explicit_coded<-explicit_coded %>%
-  mutate(FSC_M = rowMeans(x = select(.data = .,
-                                     starts_with(match = "Failure"))))
-explicit_coded<-explicit_coded %>%
-  mutate(CrSC_M = rowMeans(x = select(.data = .,
-                                      starts_with(match = "Criticism"))))
+explicit_coded <-
+  explicit_reversed %>%
+  mutate(iqms_avg = rowMeans(x = select(.data = ., starts_with(match = "iq"))))
+
+explicit_coded <- explicit_coded %>%
+  mutate(crms_avg = rowMeans(x = select(.data = ., starts_with(match = "cr_ms"))))
+
+explicit_coded <- explicit_coded %>%
+  mutate(chms_avg = rowMeans(x = select(.data = ., starts_with(match = "ch_ms"))))
+
+explicit_coded <- explicit_coded %>%
+  mutate(fms_avg = rowMeans(x = select(.data = ., starts_with(match = "fms"))))
+
+explicit_coded <- explicit_coded %>%
+  mutate(fsc_avg = rowMeans(x = select(.data = ., starts_with(match = "failure"))))
+
+explicit_coded <- explicit_coded %>%
+  mutate(crsc_avg = rowMeans(x = select(.data = ., starts_with(match = "criticism"))))
+
 #final explicit data
 explicit <-
   explicit_coded %>% 
-  select(id, gender.1, age.1, IQMS_M, CRMS_M, CHMS_M, FMS_M, FSC_M, CrSC_M, Challengescenario.1)
+  select(id, gender_1, age_1, iqms_avg, crms_avg, chms_avg, fms_avg, fsc_avg, crsc_avg, challengescenario_1)
 
 #calculating implicit mindset (D) scores by computing average rt for critical blocks separately, dividing by standard deviations across all critical blocks and subtracting positive blocks from negative blocks
 dscores <-
@@ -123,45 +127,39 @@ dscores <-
   group_by(id, target, pers_sd) %>% 
   summarise(pers_block_avg = mean(rt)) %>% 
   mutate(d = pers_block_avg/pers_sd) %>% 
-  separate(target, c("target1", "target2")) %>% 
-  select(-pers_block_avg, -pers_sd) %>% 
-  unite(targets, c("target1", "target2"), sep = "_") %>% 
-  spread(targets, d) %>% 
-  transmute(challange_d = chall_neg - chall_pos,
-            crit_d = crit_neg - crit_pos)
-
-#calculating D scores for separate blocks
-d_spread<-
-  final_gnat %>%  
-  filter(target %in% important_targets) %>%
-  filter(trial_type=="go_trial") %>% 
-  filter(error=="0") %>% 
-  group_by(id) %>% 
-  mutate(pers_sd = sd(rt)) %>% 
-  group_by(id, target, pers_sd) %>% 
-  summarise(pers_block_avg = mean(rt)) %>% 
-  mutate(d = pers_block_avg/pers_sd) %>% 
-  select(-pers_block_avg, -pers_sd) %>% 
-  spread(target, d)
+  ungroup() %>% 
+  select(-pers_block_avg, -pers_sd) %>%
+  spread(target, d) %>% 
+  mutate(challange_d = chall_neg - chall_pos,
+         crit_d = crit_neg - crit_pos)
 
 #collecting all data in one dataframe and subsetting ELTE sample
 
-final_data <- dscores %>% 
+final_data <- 
+  dscores %>% 
   left_join(explicit, by = "id")
-final_data <- d_spread %>% 
-  left_join(final_data, by = "id")
+
 
 set.seed(1)
-randomly<-
-  final_data [sample(nrow(final_data ), 86), ]
 
-write.csv(randomly, "randomly_data.csv")
+randomly <-
+  final_data %>% 
+  sample_n(86)
+  
+test_set <- 
+  final_data %>% 
+  filter(!id %in% pull(randomly, id))
+  
+
+write_excel_csv(randomly, "randomly_data.csv")
+
+sjPlot::sjp.corr(select(test_set, -id), sort.corr = FALSE)
 
 #plotting implicit citicism mindset and explicit criticism mindset
 
 install.packages("ggpubr")
 library("ggpubr")
-ggscatter(final_data, x = "crit_d", y = "CRMS_M", 
+ggscatter(final_data, x = "crit_d", y = "crms_avg", 
           add = "reg.line", conf.int = TRUE,
           cor.coef = TRUE, cor.method = "pearson",
           xlab = "Implicit Criticism Mindset", ylab = "Explicit Criticism Mindset",
